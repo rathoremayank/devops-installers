ce5e260d9d20bb2ce5bb7354fa7fe4c6cdd8091f
Mayank <47733256+rathoremayank@users.noreply.github.com>
2019-04-23 11:01:42 +0530

    Create install.sh

---
+++ jenkins-ubuntu/install.sh
@@ -0,0 +1,19 @@
+#!bin/bash
+
+#INSTALLING JENKINS 
+wget -q -O - https://pkg.jenkins.io/debian/jenkins-ci.org.key | sudo apt-key add -
+echo deb https://pkg.jenkins.io/debian-stable binary/ | sudo tee /etc/apt/sources.list.d/jenkins.list
+sudo apt-get update
+sudo apt-get install jenkins
+
+#STARTING JENKINS
+sudo systemctl start jenkins
+sudo systemctl status jenkins
+
+#OPENING FIREWALL
+sudo ufw allow 8080
+sudo ufw status
+
+echo "....................................................................."
+echo "Jenkins successfully installed.."
+echo  "visit http://ip_address_or_domain_name:8080 to start using Jenkins.." 
