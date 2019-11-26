44799b703f214d8cd9024d9d32910b57c09c1b4a
Mayank <47733256+rathoremayank@users.noreply.github.com>
2019-11-26 17:31:05 +0530

    added command for installing openjdk

--- jenkins-ubuntu.sh
+++ jenkins-ubuntu.sh
@@ -1,5 +1,8 @@
 #!bin/bash
 
+#INSTALLING JAVA 
+apt install openjdk-8-jre-headless
+
 #INSTALLING JENKINS 
 wget -q -O - https://pkg.jenkins.io/debian/jenkins-ci.org.key | sudo apt-key add -
 echo deb https://pkg.jenkins.io/debian-stable binary/ | sudo tee /etc/apt/sources.list.d/jenkins.list
